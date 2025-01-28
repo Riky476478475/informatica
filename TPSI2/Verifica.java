ee6bd2ab0bc8aa6f4b255d2bb2e134685084d019
Riky476478475 <161251209+Riky476478475@users.noreply.github.com>
2025-01-28 08:46:39 +0000

    verifica

--- TPSI2/Verifica.java
+++ TPSI2/Verifica.java
@@ -1,28 +1,34 @@
 
+
 class Veicolo {
     protected String marca;
     protected String modello;
 
-    
+
+   
     public Veicolo(String marca, String modello) {
         this.marca = marca;
         this.modello = modello;
     }
 
-    
+
+   
     public void stampaDettagli() {
         System.out.println("Marca: " + marca + ", Modello: " + modello);
     }
 }
 
+
 class Auto extends Veicolo {
     private int numeroPorte;
 
-  
+
+ 
     public Auto(String marca, String modello, int numeroPorte) {
         super(marca, modello);
         this.numeroPorte = numeroPorte;
     }
+
 
     @Override
     public void stampaDettagli() {
@@ -31,14 +37,18 @@ class Auto extends Veicolo {
 }
 
 
+
+
 class Moto extends Veicolo {
     private boolean haSidecar;
 
-    
+
+   
     public Moto(String marca, String modello, boolean haSidecar) {
         super(marca, modello);
         this.haSidecar = haSidecar;
     }
+
 
  
     @Override
@@ -48,13 +58,18 @@ class Moto extends Veicolo {
 }
 
 
-public class ES01 {
+
+
+public class Verifica {
     public static void main(String[] args) {
-      
+     
         Auto auto = new Auto("Fiat", "Panda", 5);
         Moto moto = new Moto("Harley-Davidson", "Softail", false);
+
 
         auto.stampaDettagli();
         moto.stampaDettagli();
     }
 }
+
+
