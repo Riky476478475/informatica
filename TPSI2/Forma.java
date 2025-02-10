1ce22bc701eea2685c12e4dcbc5fab82e3d61e19
Riky476478475 <161251209+Riky476478475@users.noreply.github.com>
2025-02-10 21:37:43 +0000

    asd

--- TPSI2/Forma.java
+++ TPSI2/Forma.java
@@ -1,52 +1,62 @@
-class Forma {
-    public String nome;
-
+public class Forma {
+    protected String nome;
 
     public Forma(String nome) {
         this.nome = nome;
     }
 
-
     public double calcolaArea() {
         return 0;
     }
-
 
     public double calcolaPerimetro() {
         return 0;
     }
 
-
     public void descrizione() {
-        System.out.println("Il nome della forma è: " + nome);
+        System.out.println("Questa forma è: " + nome);
+    }
+
+    public static void main(String[] args) {
+        Forma cerchio = new Cerchio(5);
+        Forma rettangolo = new Rettangolo(4, 6);
+        Forma triangolo = new Triangolo(3, 4, 3, 4, 5);
+
+        cerchio.descrizione();
+        System.out.println("Area: " + cerchio.calcolaArea());
+        System.out.println("Perimetro: " + cerchio.calcolaPerimetro());
+
+        rettangolo.descrizione();
+        System.out.println("Area: " + rettangolo.calcolaArea());
+        System.out.println("Perimetro: " + rettangolo.calcolaPerimetro());
+
+        triangolo.descrizione();
+        System.out.println("Area: " + triangolo.calcolaArea());
+        System.out.println("Perimetro: " + triangolo.calcolaPerimetro());
     }
 }
 
-
 class Cerchio extends Forma {
-    public double raggio;
-
+    private double raggio;
 
     public Cerchio(double raggio) {
         super("Cerchio");
         this.raggio = raggio;
     }
 
-
+    @Override
     public double calcolaArea() {
         return Math.PI * raggio * raggio;
     }
 
-
+    @Override
     public double calcolaPerimetro() {
         return 2 * Math.PI * raggio;
     }
 }
 
-
 class Rettangolo extends Forma {
-    public double base, altezza;
-
+    private double base, altezza;
 
     public Rettangolo(double base, double altezza) {
         super("Rettangolo");
@@ -54,21 +64,19 @@ class Rettangolo extends Forma {
         this.altezza = altezza;
     }
 
-
+    @Override
     public double calcolaArea() {
         return base * altezza;
     }
 
-
+    @Override
     public double calcolaPerimetro() {
         return 2 * (base + altezza);
     }
 }
 
-
 class Triangolo extends Forma {
-    public double base, altezza, lato1, lato2, lato3;
-
+    private double base, altezza, lato1, lato2, lato3;
 
     public Triangolo(double base, double altezza, double lato1, double lato2, double lato3) {
         super("Triangolo");
@@ -79,31 +87,13 @@ class Triangolo extends Forma {
         this.lato3 = lato3;
     }
 
-
+    @Override
     public double calcolaArea() {
         return (base * altezza) / 2;
     }
 
-
+    @Override
     public double calcolaPerimetro() {
         return lato1 + lato2 + lato3;
-    }
-}
-
-
- class Forme {
-     public static void main(String[] args) {
-        Forma[] forme = {
-            new Cerchio(5),
-            new Rettangolo(4, 6),
-            new Triangolo(3, 4, 3, 4, 5)
-        };
-
-
-        for (Forma f : forme) {
-            f.descrizione();
-            System.out.println("Area: " + f.calcolaArea());
-            System.out.println("Perimetro: " + f.calcolaPerimetro());
-        }
     }
 }
