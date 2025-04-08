ea8c994793708a2ae96c4a76e953249ae3c73a56
Riky476478475 <161251209+Riky476478475@users.noreply.github.com>
2025-04-08 08:43:59 +0000

    ret

---
+++ TPSI2/PCTO/es06.java
@@ -0,0 +1,12 @@
+public class es06 {
+    public static void main(String[] args) {
+        String strTesto = "Cavolini di Bruxelle (-25%); Fagiolini della Nonna (-50%); Kit Kat fondente (-15%)";
+
+
+        String[] prodotti = strTesto.split(";");
+
+        for (String prodotto : prodotti) {
+            System.out.println(prodotto.trim());
+        }
+    }
+}
